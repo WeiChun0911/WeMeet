089b4a5b84d4885750e660700ecb7aef0b6184b5
Wei-Chun <weichun0911@gmail.com>
2017-03-27 16:09:35 +0800

    commit before change branch
    未完成

--- server.js
+++ server.js
@@ -1,20 +1,3 @@
-/*
- * (C) Copyright 2014-2015 Kurento (http://kurento.org/)
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- */
-
 var path = require('path');
 var url = require('url');
 var cookieParser = require('cookie-parser')
@@ -28,14 +11,14 @@ var https = require('https');
 
 var argv = minimist(process.argv.slice(2), {
     default: {
-        as_uri: 'https://localhost:8443/',
+        as_uri: 'https://localhost:8443',
         ws_uri: 'ws://localhost:8888/kurento'
     }
 });
 
 var options = {
-    key: fs.readFileSync('keys/server.key'),
-    cert: fs.readFileSync('keys/server.crt')
+    key: fs.readFileSync('certificate/privatekey.pem'),
+    cert: fs.readFileSync('certificate/certificate.pem')
 };
 
 var app = express();
@@ -274,4 +257,4 @@ function onIceCandidate(sessionId, _candidate) {
     }
 }
 
-app.use(express.static(path.join(__dirname, 'static')));
+app.use(express.static(path.join(__dirname, 'public/')));
