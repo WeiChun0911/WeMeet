23d526323f1c4cf7b0bc077ec447aea032333799
Wei-Chun <weichun0911@gmail.com>
2017-04-10 12:25:15 +0800

    4/10小更新
    1.新增public路徑
    2.路徑改為https://.......html#<由此做routing>?<由此做QueryString name=...>
    3.下一步進入函數包裝與API設計

--- index.js
+++ index.js
@@ -6,9 +6,12 @@ const app = express();
 const fs = require('fs');
 //HTTPS參數
 const option = {
-    key: fs.readFileSync('./certificate/privatekey.pem'),
-    cert: fs.readFileSync('./certificate/certificate.pem')
+    key: fs.readFileSync('./public/certificate/privatekey.pem'),
+    cert: fs.readFileSync('./public/certificate/certificate.pem')
 };
+
+var connection = {};
+
 //對https Server內傳入express的處理物件
 const server = require('https').createServer(option, app);
 const io = require('socket.io')(server);
@@ -17,19 +20,28 @@ console.log('已啟動伺服器!');
 
 let counter = 0;
 app.get('', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/public/index.html');
 })
-app.get('https://localhost:8787/chat.html', (req, res) => {
-    res.sendFile(__dirname + '/chat.html');
+app.get('https://140.123.175.95:8787/public/chat.html', (req, res) => {
+    res.sendFile(__dirname + '/public/chat.html');
 })
 
-app.use(express.static('./'));
+app.use(express.static('public'));
+
 
 io.on('connection', function(socket) {
+    connection[socket.id] = socket;
     console.log("接收到使用者: " + socket.id + " 的連線");
 
+    socket.on('create',function(){
+        console.log('收到創建房間: ' + room + ' 的請求');
+        socket.join(room);
+        console.log('Client ID ' + socket.id + ' joined room ' + room);
+        socket.emit('joined', room, socket.id);
+    })
+
     socket.on('join', function(room) {
-        console.log('收到創建/加入房間: ' + room + ' 的請求');
+        console.log('收到「加入」房間: ' + room + ' 的請求');
         socket.join(room);
         console.log('Client ID ' + socket.id + ' joined room ' + room);
         socket.emit('joined', room, socket.id);
@@ -50,6 +62,11 @@ io.on('connection', function(socket) {
     socket.on('onIceCandidate', function(candidate, sender, receiver) {
         socket.to(receiver).emit('onIceCandidate', candidate, sender);
     })
+
+    socket.on('disconnect', function() {
+        console.log("使用者: "+ socket.id + " 離開了");
+        socket.broadcast.emit('participantLeft',socket.id);
+    });
 
     socket.on('bye', function() {
         console.log('received bye');
